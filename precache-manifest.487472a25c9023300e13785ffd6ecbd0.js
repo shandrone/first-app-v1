d6e87765059a12a6013870206685ce30a0c9ba92
shandrone <shandrone@gmail.com>
2020-09-29 23:01:58 -0600

    Updates

---
+++ precache-manifest.487472a25c9023300e13785ffd6ecbd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e703ac2d072fd1762d5e04bf8f067d",
+    "url": "/first-app-v1/index.html"
+  },
+  {
+    "revision": "46c761d64d34574a18e2",
+    "url": "/first-app-v1/static/css/main.dc35676c.chunk.css"
+  },
+  {
+    "revision": "ebfc4ac51a01d619fdde",
+    "url": "/first-app-v1/static/js/2.f62ae7c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first-app-v1/static/js/2.f62ae7c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c761d64d34574a18e2",
+    "url": "/first-app-v1/static/js/main.411a3311.chunk.js"
+  },
+  {
+    "revision": "fe7cbc8aa5e022fb1b00",
+    "url": "/first-app-v1/static/js/runtime-main.af003945.js"
+  }
+]);
